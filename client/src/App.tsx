4e8fc7915d43778dbaa2ace4fee811a74b7632d8
tuheli <elias.tuhola@gmail.com>
2024-09-28 19:45:57 +0300

    Renamed folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import { Redirect } from "./components/redirect/Redirect";
-import { ExamplePage } from "./components/example-page/ExamplePage";
+import { ExamplePage } from "./components/pages/ExamplePage";
 import { ThemeProvider } from "./components/theme-provider/ThemeProvider";
 
 export const App = () => {
