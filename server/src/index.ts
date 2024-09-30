a1bb154c11b729594bd8e882951f98e92872ef39
tuheli <elias.tuhola@gmail.com>
2024-09-30 15:12:35 +0300

    Removed endpoints and database connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,6 @@
-import "./config";
 import express from "express";
 import cors from "cors";
 import path from "path";
-import { connectToDatabase } from "./database/database";
-import signUpRouter from "./routers/signUpRouter";
-import signInRouter from "./routers/signInRouter";
-import validateAuthToken from "./routers/validateAuthTokenRouter";
 
 const port = 3000;
 const relativeDistPath =
@@ -17,9 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", express.static(absoluteDistPath));
-app.use("/api/signup", signUpRouter);
-app.use("/api/signin", signInRouter);
-app.use("/api/validateauthorizationtoken", validateAuthToken);
 app.get("/*", (req, res) => {
   if (req.originalUrl.startsWith("/api")) {
     return res.status(404).json({ message: "Unknown endpoint." });
@@ -30,8 +22,6 @@ app.get("/*", (req, res) => {
 });
 
 const start = async () => {
-  await connectToDatabase();
-
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
